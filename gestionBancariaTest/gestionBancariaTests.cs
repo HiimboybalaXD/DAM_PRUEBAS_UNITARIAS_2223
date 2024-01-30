5d0a1efe9afb0b0aa73390c40333c9e57f2495fb
HiimboybalaXD <130750949+HiimboybalaXD@users.noreply.github.com>
2024-01-30 16:57:08 +0100

    Dia1

---
+++ gestionBancariaTest/gestionBancariaTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using gestionBancariaApp;
+using System;
+
+namespace gestionBancariaTest
+{
+    [TestClass]
+    public class gestionBancariaTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
